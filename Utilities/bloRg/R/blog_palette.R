f6340655e9310e061db0a24244591a310d5d8656
cormac85 <cormac85@gmail.com>
2018-04-10 19:57:04 +0100

    blorg can now be loaded correctly during dev.

--- Utilities/bloRg/R/blog_palette.R
+++ Utilities/bloRg/R/blog_palette.R
@@ -1,6 +1,12 @@
 #' Blog Colour Palette
 #'
-#' A vector containing RGB values for the colours for my blog
+#' A vector containing RGB values for the colours for my blog.
+#'
+#' Create with:
+#' blog_palette = c("#AF240B", "#0B556D","#AF540B", "#088031", # normal
+#' +          "#E33048", "#31778E", "#E39848", "#35065B", # Light
+#' +          "#E3304850", "#31778E50", "#E3984850", "#35A65B50") # Super Light
+#' save(blog_palette, file = "./data/blog_palette.rda")
 #'
 #' @author Cormac Nolan
 #' @keywords data
